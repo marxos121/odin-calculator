311269f574d5bda1fd3d4cef6de9610493c718bd
marxos121 <marxos121@gmail.com>
2023-02-16 15:10:32 +0100

    add JS calculator object

--- scripts.js
+++ scripts.js
@@ -22,3 +22,36 @@ function operate(a, b, symbol){
         case '/': return divide(a, b); break;
     }
 }
+
+let calc = {
+    displayString: '',
+    firstNumber: 0,
+    secondNumber: 0,
+    symbol: '',
+    result: 0,
+
+    add: add(),
+    subtract: subtract(),
+    multiply: multiply(),
+    divide: divide(),
+    operate: operate(),
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", button, () => {
+        if(+button.textContent){
+            if(!calc.symbol){
+                firstNumber = +button.textContent;
+            } else {
+                secondNumber = +button.textContent;
+            }
+        }
+        else if(button.textContent === '='){
+            calc.operate();
+        }
+        else {
+            calc.symbol = text.textContent;
+        }
+    }) 
+});
