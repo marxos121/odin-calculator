59588f78a194420a0741f6b04ccfdf519b36024b
marxos121 <marxos121@gmail.com>
2023-02-16 17:20:13 +0100

    fixed bug where numbers were concatenated to result

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const calc = {
     secondNumber: '',
     symbol: '',
     result: 0,
+    newFirst: true,
 
     add: function() { this.result = +this.firstNumber + +this.secondNumber; },
     subtract: function() { this.result = +this.firstNumber - +this.secondNumber; },
@@ -23,6 +24,7 @@ const calc = {
         
         this.displayString = this.result;
         this.result = 0;
+        this.newFirst = true;
         this.updateDisplay();
     },
     updateDisplay: function() {
@@ -33,14 +35,19 @@ const calc = {
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
+        if(calc.displayString == '0' || calc.newFirst === true) { calc.displayString = ''; }
         if(+button.textContent || button.textContent === '0'){
             if(!calc.symbol){                           //If we don't yet have a symbol, treat new input as the first number
+                console.log(calc.newFirst);
+                if(calc.newFirst){
+                    calc.firstNumber = '';
+                    calc.newFirst = false;
+                }
                 calc.firstNumber += button.textContent;
             } else {
                 calc.secondNumber += button.textContent;
             }
 
-            if(calc.displayString == '0' || calc.displayString == calc.result) { calc.displayString = ''; }
             calc.displayString += button.textContent;
         }
         else if(button.textContent == '='){
